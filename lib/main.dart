ce358aecb5647afbbafcd494df742887e43af2c5
ErykTavares <erictavares35@gmail.com>
2026-01-01 13:32:30 -0300

    feat(main): update MyHomePage layout with GradienteContainer and DiceRoller

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/shared/widgets/color_box.dart';
+import 'package:flutter_app/shared/widgets/dice_roller.dart';
+import 'package:flutter_app/shared/widgets/gradiente_container.dart';
 import 'package:flutter_app/shared/widgets/image_wrapper.dart';
 
 void main() {
@@ -42,36 +44,50 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Scrollbar(
           thumbVisibility: true,
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 20,
-              children: <Widget>[
-                ColorBox(color: Colors.red, text: '1'),
-                ColorBox(color: Colors.purple, text: '2'),
-                ColorBox(color: Colors.blue, text: '3'),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(vertical: 0),
-                  child: Column(
-                    spacing: 0,
-                    children: [
-                      ImageWrapper(
-                        imagePath:
-                            'assets/images/HD-wallpaper-berserk-guts-badass-anime-thumbnail.jpg',
-                      ),
-                      ImageWrapper(
-                        imagePath:
-                            'assets/images/new-berserk-wallpapers-upscaled-to-8k-then-compressed-to-5-v0-z0120ermi47f1.jpg',
-                      ),
-                      ImageWrapper(
-                        imagePath:
-                            'assets/images/the-black-swordsman-berserk-thumb-728x410.jpg',
-                      ),
-                    ],
+            child: GradienteContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                spacing: Magnifier.kStandardVerticalFocalPointShift,
+                children: [
+                  Text(
+                    "Dice Game",
+                    style: TextStyle(fontSize: 30, color: Colors.black87),
                   ),
-                ),
-              ],
+                  DiceRoller(),
+                ],
+              ),
             ),
+            // child: Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   spacing: 20,
+            //   children: <Widget>[
+            // ColorBox(color: Colors.red, text: '1'),
+            // ColorBox(color: Colors.purple, text: '2'),
+            // ColorBox(color: Colors.blue, text: '3'),
+            // Container(
+            //   alignment: Alignment.center,
+            //   margin: EdgeInsets.symmetric(vertical: 0),
+            //   child: Column(
+            //     spacing: 0,
+            //     children: [
+            //       ImageWrapper(
+            //         imagePath:
+            //             'assets/images/HD-wallpaper-berserk-guts-badass-anime-thumbnail.jpg',
+            //       ),
+            //       ImageWrapper(
+            //         imagePath:
+            //             'assets/images/new-berserk-wallpapers-upscaled-to-8k-then-compressed-to-5-v0-z0120ermi47f1.jpg',
+            //       ),
+            //       ImageWrapper(
+            //         imagePath:
+            //             'assets/images/the-black-swordsman-berserk-thumb-728x410.jpg',
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            // ],
+            // ),
           ),
         ),
       ),
