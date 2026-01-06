ec1a5b5ab4fdc877f450adb9a6a8f8e324721fab
ErykTavares <erictavares35@gmail.com>
2026-01-05 22:15:43 -0300

    feat(main): simplify main application structure

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Quiz App',
       theme: ThemeData(primaryColor: const Color.fromARGB(255, 115, 13, 5)),
-      home: const MyHomePage(title: ''),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Color.fromARGB(255, 157, 18, 8)),
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: MainScreen(),
-        ),
-      ),
+      home: Scaffold(body: MainScreen()),
     );
   }
 }
