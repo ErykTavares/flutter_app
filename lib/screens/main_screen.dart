5a5a018d86780899b39a7cf4292febed13ad5319
ErykTavares <erictavares35@gmail.com>
2026-01-05 22:15:19 -0300

    feat(main_screen): refactor MainScreen layout and button functionality

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,54 +5,67 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      height: double.infinity,
-      alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(vertical: 150, horizontal: 10),
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color.fromARGB(255, 157, 18, 8),
-            Color.fromARGB(255, 198, 30, 18),
-          ],
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 40,
-        children: [
-          Image.asset('assets/images/quiz_logo.png', width: 300, height: 300),
-          Text(
-            "Learn Flutter the fun way!",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-              color: Colors.white,
-            ),
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-              alignment: Alignment.center,
-              backgroundColor: WidgetStateProperty.all<Color>(
-                Color.fromARGB(255, 198, 30, 18),
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color.fromARGB(255, 157, 18, 8)),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Container(
+            width: double.maxFinite,
+            height: double.infinity,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: 150, horizontal: 10),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 157, 18, 8),
+                  Color.fromARGB(255, 198, 30, 18),
+                ],
               ),
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 40,
+              children: [
+                Image.asset(
+                  'assets/images/quiz_logo.png',
+                  width: 300,
+                  height: 300,
                 ),
-              ),
-            ),
-            onPressed: () => {},
-            child: const Text(
-              "Start Quiz",
-              style: TextStyle(fontSize: 20, color: Colors.white),
+                Text(
+                  "Learn Flutter the fun way!",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    alignment: Alignment.center,
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      Color.fromARGB(255, 198, 30, 18),
+                    ),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => {},
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
