0e23c2d0920bacf9e923061c7e2ada7f7af96d93
ErykTavares <Erictavares35@gmail.com>
2026-02-12 02:15:32 -0300

    feat(answers_button): add AnswersButton widget for quiz answer selection

---
+++ lib/shared/widgets/anwsers_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AnswersButton extends StatelessWidget {
+  const AnswersButton({super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+      ),
+      child: Text(text, style: TextStyle(fontWeight: FontWeight.w700)),
+    );
+  }
+}
