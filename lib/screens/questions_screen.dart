e505f0cacd1037fc8fa2d4a21f9f982e7f9489f1
ErykTavares <Erictavares35@gmail.com>
2026-02-12 02:15:23 -0300

    feat(questions_screen): implement QuestionScreen layout with answer buttons

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/shared/widgets/anwsers_button.dart';
+import 'package:flutter_app/shared/widgets/layout.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +14,17 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Text("Question Screen");
+    return Layout(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          AnswersButton(onPressed: () {}, text: 'Answer 1'),
+          AnswersButton(onPressed: () {}, text: 'Answer 2'),
+          AnswersButton(onPressed: () {}, text: 'Answer 3'),
+        ],
+      ),
+    );
   }
 }
