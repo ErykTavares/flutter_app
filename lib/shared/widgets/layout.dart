a7a4ea2bb9ad4a483eda8822ecd90eca4ea0918d
Eryk Tavares <EricTavares35@gmail.com>
2026-02-13 01:55:59 -0300

    feat(layout): add padding parameter to Layout widget

--- lib/shared/widgets/layout.dart
+++ lib/shared/widgets/layout.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Layout extends StatelessWidget {
-  const Layout({super.key, required this.child});
+  const Layout({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.symmetric(horizontal: 10),
+  });
 
   final Widget child;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +17,7 @@ class Layout extends StatelessWidget {
         width: double.maxFinite,
         height: double.infinity,
         alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: padding,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
