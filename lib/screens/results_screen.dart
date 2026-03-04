bac08c65b04359cfb24db09f42537219f58b069e
Eryk Tavares <EricTavares35@gmail.com>
2026-03-04 02:16:07 -0300

    feat(results_screen): enhance ResultsScreen with quiz summary and restart functionality

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/data/questions.dart';
 import 'package:flutter_app/shared/widgets/button.dart';
 import 'package:flutter_app/shared/widgets/layout.dart';
+import 'package:flutter_app/shared/widgets/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+  });
+
+  final List<String> chosenAnswers;
+  final VoidCallback onRestart;
+
+  List<Map<String, Object>> getSummaryData() {
+    return List.generate(chosenAnswers.length, (i) {
+      final currentQuestion = questions[i];
+
+      return {
+        'question_index': i,
+        'question': currentQuestion.text,
+        'correct_answer': currentQuestion.answers[0],
+        'chosen_answer': chosenAnswers[i],
+      };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final totalQuestions = questions.length;
+    final correctAnswers = summaryData.where((item) {
+      return item['chosen_answer'] == item['correct_answer'];
+    }).length;
+
     return Layout(
       child: Center(
         child: Column(
@@ -14,13 +42,15 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text(
+              'You answered $correctAnswers out of $totalQuestions questions correctly!',
+            ),
             const SizedBox(height: 30),
-            const Text('List of answers and questions'),
+            QuestionsSummary(summaryData: summaryData),
             const SizedBox(height: 30),
             Button(
               text: "Restart Quiz",
-              onPressed: () => {},
+              onPressed: onRestart,
               icon: Icons.restart_alt,
             ),
           ],
