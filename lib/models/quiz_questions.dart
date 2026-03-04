1d9f6c72b30f614ecb58b87ad1b0dafb8b34ccd0
Eryk Tavares <EricTavares35@gmail.com>
2026-03-04 02:36:27 -0300

    feat(quiz_questions): refactor getShuffledAnswers method to a getter

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -4,7 +4,7 @@ class QuisQuestion {
   final String text;
   final List<String> answers;
 
-  List<String> getShuffledAnswers() {
+  List<String> get shuffledAnswers {
     final shuffledAnswers = List.of(answers);
     shuffledAnswers.shuffle();
 
